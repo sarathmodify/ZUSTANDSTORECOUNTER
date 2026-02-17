7f2126b8a9956c98766eda15b9381c2c08fd3116
sarathmanikam <sarathmanikam369@gmail.com>
2026-02-17 21:33:21 +0530

    feat: counter inc/dec value using zustand

---
+++ vite-project/src/store/useCounterStore.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+
+type CounterState = {
+    count:number;
+    increment: () => void;
+    decrement: () => void;
+    reset: () => void;
+}
+
+export const counterStore = create<CounterState>((set) => ({
+    count: 0,
+    increment: () => set((state) => ({ count: state.count + 1 })),
+    decrement: () => set((state) => ({ count: state.count - 1 })),
+    reset: () => set({ count: 0 }),
+}));
+
