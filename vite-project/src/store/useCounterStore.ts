1d472282dba04370a9133fb0f226c9c29636076c
sarathmanikam <sarathmanikam369@gmail.com>
2026-02-18 15:52:44 +0530

    feat: implement a persistent Zustand counter store with increment, decrement, and reset actions.

--- vite-project/src/store/useCounterStore.ts
+++ vite-project/src/store/useCounterStore.ts
@@ -1,16 +1,21 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type CounterState = {
-    count:number;
+    count: number;
     increment: () => void;
     decrement: () => void;
     reset: () => void;
 }
 
-export const counterStore = create<CounterState>((set) => ({
+export const counterStore = create<CounterState>()(persist((set) => ({
     count: 0,
     increment: () => set((state) => ({ count: state.count + 1 })),
     decrement: () => set((state) => ({ count: state.count - 1 })),
     reset: () => set({ count: 0 }),
-}));
+}),
+    {
+        name: 'counter'
+    }
+));
 
